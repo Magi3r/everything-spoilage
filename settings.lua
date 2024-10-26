fe398ca9f044267b76d5dd3698504993fe695bd2
Magi3r <jvd@outlook.de>
2024-10-27 01:52:37 +0200

    feat: add settings for spoil_time

---
+++ settings.lua
@@ -0,0 +1,11 @@
+-- Settings
+data:extend({
+    {
+        type = "double-setting",
+        name = "everything-spoilage-spoil_time",
+        localised_name = {"everything-spoilage-settings.spoil_time_name"},
+        localised_description = {"everything-spoilage-settings.spoil_time_description"},
+        setting_type = "startup",
+        default_value = 60.0
+    }
+})
