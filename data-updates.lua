941adf5e7741ce9758bc159b550dc1d0f337255e
Magi3r <jvd@outlook.de>
2024-10-27 01:52:37 +0200

    feat: add settings for spoil_time

--- data-updates.lua
+++ data-updates.lua
@@ -1,6 +1,6 @@
 for key, value in pairs(data.raw.item) do
     if not value.spoil_result then
         value.spoil_result = "spoilage"
-        value.spoil_ticks = 1 * hour
+        value.spoil_ticks = settings.startup["everything-spoilage-spoil_time"].value * minute
     end
 end
