22f43930efbd6f6bfd989926a964d732ddc3209e
Gladon4 <alex.mario.grille.amg@gmail.com>
2024-11-21 23:35:03 +0100

    other: Restruture of Code for future work

--- data-updates.lua
+++ data-updates.lua
@@ -1,9 +1,9 @@
 if (settings.startup["everything-spoilage-mod_mode"].value == "random") then
     require("prototypes.random-spoilage")
--- else if (settings.startup["everything-spoilage-mod_mode"].value == "normal") then
---     require("prototypes.spoilage")
---     require("prototypes.item")
---     require("prototypes.item_groups")
---     require("prototypes.recipe")
--- end
+else if (settings.startup["everything-spoilage-mod_mode"].value == "normal") then
+    require("prototypes.item")
+    require("prototypes.spoilage")
+    require("prototypes.item_groups")
+    require("prototypes.recipe")
+end
 end
--- prototypes/spoilage.lua
+++ prototypes/spoilage.lua
@@ -1,13 +1,31 @@
 -- todo: spoil times
-data.raw["item"]["iron-plate"].spoil_result = "rusted-iron-plate"
-data.raw["item"]["iron-plate"].spoil_ticks = 1 * hour
+local spoilage = 
+{
+  {"iron-plate", "rusted-iron-plate", 30*minute},
+  {"copper-plate", "rusted-copper-plate", 30*minute},
+  {"steel-plate", "rusted-steel-plate", 5*hour},
+  {"rusted-iron-plate", "basic-metal-scrap", 15*minute},
+  {"rusted-copper-plate", "basic-metal-scrap", 15*minute},
+  {"rusted-steel-plate", "basic-metal-scrap", 45*minute},
+  {"advanced-metal-scrap", "basic-metal-scrap", 60*minute},
+  {"electronic-scrap", "basic-metal-scrap", 60*minute},
+  {"oil-scrap", "spoilage", 60*minute},
+  {"iron-gear-wheel", "basic-metal-scrap", 30*minute},
+  {"iron-stick", "basic-metal-scrap", 30*minute},
+  {"copper-cable", "basic-metal-scrap", 30*minute},
+  {"barrel", "basic-metal-scrap", 30*minute},
+  {"pipe", "basic-metal-scrap", 30*minute},
+  {"pipe-to-ground", "basic-metal-scrap", 30*minute},
+} 
 
-data.raw["item"]["copper-plate"].spoil_result = "rusted-copper-plate"
-data.raw["item"]["copper-plate"].spoil_ticks= 1 * hour
-
-for key, value in pairs(data.raw.item) do
-  if not value.spoil_result then
-      value.spoil_result = "spoilage"
-      value.spoil_ticks = settings.startup["everything-spoilage-spoil_time"].value * minute
-  end
+for key, value in pairs(spoilage) do
+  data.raw["item"][value[1]].spoil_result = value[2]
+  data.raw["item"][value[1]].spoil_ticks = value[3]
 end
+
+-- for key, value in pairs(data.raw.item) do
+--   if not value.spoil_result then
+--       value.spoil_result = "spoilage"
+--       value.spoil_ticks = settings.startup["everything-spoilage-spoil_time"].value * minute
+--   end
+-- end
