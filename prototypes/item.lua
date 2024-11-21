22b7d2bf1e86d7a7a5c77078be08832ef282892c
Gladon4 <alex.mario.grille.amg@gmail.com>
2024-11-21 23:34:28 +0100

    feat: added intermediate spoilage items

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -6,8 +6,6 @@ data:extend(
         icon = "__everything-spoilage__/graphics/icons/rusted-iron-plate.png",
         icon_size = 64,
         stack_size = 100,
-        spoil_result = "basic-metal-scrap",
-        spoil_ticks = 1 * minute,
         subgroup = "spoilage-items",
         weight = 1 * kg
     },
@@ -17,8 +15,15 @@ data:extend(
         icon = "__everything-spoilage__/graphics/icons/rusted-copper-plate.png",
         icon_size = 64,
         stack_size = 100,
-        spoil_result = "basic-metal-scrap",
-        spoil_ticks = 1 * minute,
+        subgroup = "spoilage-items",
+        weight = 1 * kg
+    },
+    {
+        type = "item",
+        name = "rusted-steel-plate",
+        icon = "__everything-spoilage__/graphics/icons/rusted-steel-plate.png",
+        icon_size = 64,
+        stack_size = 100,
         subgroup = "spoilage-items",
         weight = 1 * kg
     },
@@ -28,6 +33,36 @@ data:extend(
         icon = "__space-age__/graphics/icons/scrap.png", --todo: icon
         icon_size = 64,
         stack_size = 100,
+        subgroup = "spoilage-items",
+        weight = 1 * kg
+    },
+    {
+        type = "item",
+        name = "advanced-metal-scrap",
+        icon = "__space-age__/graphics/icons/scrap.png", --todo: icon
+        icon_size = 64,
+        stack_size = 100,
+        subgroup = "spoilage-items",
+        weight = 1 * kg
+    },
+    {
+        type = "item",
+        name = "electronic-scrap",
+        icon = "__space-age__/graphics/icons/scrap.png", --todo: icon
+        icon_size = 64,
+        stack_size = 100,
+        subgroup = "spoilage-items",
+        weight = 1 * kg
+    }
+    ,
+    {
+        type = "item",
+        name = "oil-scrap",
+        icon = "__base__/graphics/icons/fluid/heavy-oil.png", --todo: icon
+        icon_size = 64,
+        stack_size = 100,
+        spoil_result = "spoilage",
+        spoil_ticks = 1 * minute,
         subgroup = "spoilage-items",
         weight = 1 * kg
     }
