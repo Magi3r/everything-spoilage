111c83f56fc4fafafc5febe014e0c5b5d1b0a626
Gladon4 <alex.mario.grille.amg@gmail.com>
2024-11-28 18:58:49 +0100

    other: Some under the hood changes

--- prototypes/random-spoilage.lua
+++ prototypes/random-spoilage.lua
@@ -1,5 +1,5 @@
 require("prototypes.function")
-set_seed(settings.startup["random-spoilage-seed"].value)
+set_seed(settings.startup["everything-spoilage_random-spoilage-seed"].value)
 
 local random_item_blacklist = {
     "coin",
@@ -70,7 +70,7 @@ for _, category in pairs(categories) do
             -- This log is used for the graphing script
             -- log(item.name .. ", " .. spoil_result)
             item.spoil_result = spoil_result
-            local spoil_minute =  math.floor(settings.startup["random-spoilage-min-spoil-time"].value+(settings.startup["random-spoilage-max-spoil-time"].value - settings.startup["random-spoilage-min-spoil-time"].value)*randomValue()^3)
+            local spoil_minute =  math.floor(settings.startup["everything-spoilage_random-spoilage-min-spoil-time"].value+(settings.startup["everything-spoilage_random-spoilage-max-spoil-time"].value - settings.startup["random-spoilage-min-spoil-time"].value)*randomValue()^3)
             item.spoil_ticks = spoil_minute * minute
         end
     end
