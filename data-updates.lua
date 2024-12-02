64b69ffab1b834d71324d9011f25458a5c24cc21
Magi3r <jvd@outlook.de>
2024-12-02 19:20:59 +0100

    compat: define items and recipes earlier

--- data-updates.lua
+++ data-updates.lua
@@ -1,31 +1,6 @@
 ------------- PEAK GLEBA MODE -------------
 if settings.startup["everything-spoilage_mod-mode"].value == "peak-gleba" then
-    -- Make space age optionally required
-    local spoil_result = "wood"
-    if mods["space-age"] then
-        spoil_result = "spoilage"
-    end
-
-    local categories = {
-        "item",
-        "tool",
-        "module",
-        "item-with-entity-data",
-        "rail-planner",
-        "ammo",
-        "gun",
-        "armor",
-        "capsule",
-    }
-
-    for _, category in pairs(categories) do
-        for _, item in pairs(data.raw[category]) do
-            if not item.spoil_result and not item.hidden and item.name ~= spoil_result then
-                item.spoil_result = spoil_result
-                item.spoil_ticks = settings.startup["everything-spoilage_spoil-time"].value * minute
-            end
-        end
-    end
+    require("prototypes.peak-gleba-spoilage")
 end
 
 ------------- RANDOM MODE -------------
@@ -35,9 +10,6 @@ end
 
 ------------- NORMAL MODE -------------
 if (settings.startup["everything-spoilage_mod-mode"].value == "normal") then
-    require("prototypes.item")
-    require("prototypes.spoilage")
-    require("prototypes.item_groups")
-    require("prototypes.recipe")
+    require("prototypes.normal-spoilage")
 end
 
